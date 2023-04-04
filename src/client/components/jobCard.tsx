12ee7b82dbc4ad16795b0201e72d17d91d4e2e3e
anthonyrvega <103469512+anthonyrvega@users.noreply.github.com>
2023-04-03 23:35:57 -0700

    job card and jobFeedContainer debugged

--- src/client/components/jobCard.tsx
+++ src/client/components/jobCard.tsx
@@ -1,37 +1,51 @@
-import React from "react";
 
-//define two interfaces 
-//#1 jobCard 
-export interface JobCard {
-        id: number,
-        datePosted : number,
-        title: string,
-        company: string, 
-        location: string, 
-        description: string,
-        remote: boolean,
-        requirements: string, 
-        salary: number
-    }
-//#2 jobCardProps - props that jobCard will accept
-export interface jobCardProps{
-    jobCard: JobCard;
+import React, { useState } from 'react';
+
+interface JobCardProps {
+  id: number;
+  datePosted: string;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  remote: boolean;
+  requirements: string;
+  salary: number;
 }
 
-//declare job card function 
-export const JobCard = ({jobCard}: jobCardProps) =>{
-    return (
-    <div className = 'job-card'>
-        <h1>{jobCard.title}</h1>
-        <h2>{jobCard.company}</h2>
-        <h2>{jobCard.datePosted}</h2>
-        <p>{jobCard.location}</p>
-        <p>{jobCard.description}</p>
-        <p>{jobCard.remote}</p>
-        <p>{jobCard.requirements}</p>
-        <p>{jobCard.salary}</p>
+const JobCard: React.FC<JobCardProps> = ({
+  id,
+  datePosted,
+  title,
+  company,
+  location,
+  description,
+  remote,
+  requirements,
+  salary,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{company}</p>
+      <p>{location}</p>
+      <p>{salary}</p>
+      <p>{datePosted}</p>
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? 'Hide Details' : 'View Details'}
+      </button>
+      {isExpanded && (
+        <div>
+          <p>{description}</p>
+          <p>{requirements}</p>
+          <p>{remote ? 'Remote' : 'Not remote'}</p>
+        </div>
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default JobCard;
+
--- src/client/containers/jobFeedContainer.tsx
+++ src/client/containers/jobFeedContainer.tsx
@@ -1,46 +1,56 @@
-import React from "react";
-import  JobCard from "../components/jobCard"; 
+// jobFeedContainer.tsx
+import React, { useState } from 'react';
+import JobCard from '../components/jobCard';
 
-
-// interface Job {
-//         id: number,
-//         datePosted : number,
-//         title: string,
-//         company: string, 
-//         location: string, 
-//         description: string,
-//         remote: boolean,
-//         requirements: string, 
-//         salary: number
-// }
-//render jobCard into the feed container 
-interface jobFeedContainerProps {
-    jobCards: JobCard[];
+interface Job {
+  id: number;
+  datePosted: string;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  remote: boolean;
+  requirements: string;
+  salary: number;
 }
-// const JobFeedContainer = ({jobCards}: jobFeedContainerProps) => {
-//   const fakeJob = {
-//     id: 1,
-//     datePosted : Date.now(),
-//     title: 'Software Engineer',
-//     company: 'Raytheon', 
-//     location: 'Oklahoma', 
-//     description: 'building bombs',
-//     remote: false,
-//     requirements: 'javascript', 
-//     salary: 300000
-//   };
-//testjobs 
 
+const JobFeedContainer: React.FC = () => {
+  const [jobs, setJobs] = useState<Job[]>([]);
 
-  const JobFeedContainer = ({ jobCards }: jobFeedContainerProps) => {
-    return (
-      <div className="job-feed-container">
-        {jobCards.map((jobCard) => (
-          <JobCard key={jobCard.title} jobCard={jobCard} />
-        ))}
-      </div>
-    );
+  const handleAddJob = (job: Job) => {
+    setJobs([...jobs, job]);
   };
+//example job added
+  return (
+    <div>
+      <h1>Job Feed</h1>
+      <button onClick={() => handleAddJob({
+        id: 1,
+        datePosted: 'April 1, 2023',
+        title: 'React Developer',
+        company: 'Acme Co.',
+        location: 'Remote',
+        description: 'We are seeking a talented React developer to join our team.',
+        remote: true,
+        requirements: 'Minimum 3 years experience with React.',
+        salary: 80000,
+      })}>Add Job</button>
+      {jobs.map(job => (
+        <JobCard
+          key={job.id}
+          id={job.id}
+          datePosted={job.datePosted}
+          title={job.title}
+          company={job.company}
+          location={job.location}
+          description={job.description}
+          remote={job.remote}
+          requirements={job.requirements}
+          salary={job.salary}
+        />
+      ))}
+    </div>
+  );
+};
 
-
-module.exports = JobFeedContainer;
+export default JobFeedContainer;
