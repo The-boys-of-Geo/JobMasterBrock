0cd459ef49842f06e27fe62c6034f24a92f01489
anthonyrvega <103469512+anthonyrvega@users.noreply.github.com>
2023-04-05 13:52:47 -0700

    drag n drop basics

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,26 +1,38 @@
 import React, { useState, useEffect, Fragment } from 'react';
 const styles = require('./App.css');
-
+import { YourJobs } from './pages/YourJobs';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { JobPage } from './pages/JobPage';
-import { ApplicationsPage } from './pages/ApplicationsPage'
+//import { ApplicationsPage } from './pages/ApplicationsPage'
 import { InterviewPage } from './pages/InterviewPage'
-import AuthModal from './components/AuthModal'
 
 function App() {
-  const [signedIn, setSignedIn] = useState(false);
+
 
 
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<AuthModal />} />
-        <Route path="/" element={ <JobPage />} />
-        {/* <Route path="/applications" element={ <ApplicationsPage /> } /> */}
-        <Route path="/interviews" element={ <InterviewPage />} />
+      <Route path="/" element={ <JobPage />} />
+      <Route path="/applications" element={ <YourJobs />} />
+      <Route path="/interviews" element={ <InterviewPage />} />
       </Routes>
+       
+      
     </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <YourJobs/>
+//       {/* <JobPage /> */}
+//       {/* <ApplicationsPage /> */}
+//       {/* <InterviewPage /> */}
+//     </div>
+//   );
+// }
+
 
 export default App;
